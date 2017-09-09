bb036ab670eaaa08d05176c6bfa81bde114c4e01
Christopher LaPointe <cmlapointe11@gmail.com>
2017-09-09 15:55:07 -0400

    Setup logger, rewrite gdax implementation, add polling

--- exchanges/gdax/index.js
+++ exchanges/gdax/index.js
@@ -1,12 +1,54 @@
-const gdax = require('gdax');
 const Promise = require('bluebird');
+const axios = require('axios');
+const crypto = require('crypto');
+const _ = require('lodash')
+const config = require('./config');
 
-module.exports = (config) => {
-	const client = Promise.promisifyAll(new gdax.AuthenticatedClient(config.key, config.b64secret, config.passphrase));
+function createSignedHeadersAsync(epoch, method, uri, serializedBody) {
+	return new Promise((resolve, reject) => {
+		const hmac = crypto.createHmac('sha256', new Buffer(config.b64secret, 'base64'));
+		hmac.on('readable', () => {
+			const data = hmac.read();
+			if (data) {
+				resolve({
+					'CB-ACCESS-KEY' : config.key,
+					'CB-ACCESS-SIGN' : data.toString('base64'),
+					'CB-ACCESS-TIMESTAMP' : epoch,
+					'CB-ACCESS-PASSPHRASE' : config.passphrase,
+				})
+			} else {
+				reject(new Error('Error creating signature headers'));
+			}
+		});
+		hmac.write('' + epoch);
+		hmac.write(method.toUpperCase());
+		hmac.write(uri);
+		if (serializedBody)
+			hmac.write(serializedBody);
+		hmac.end();
+	});
+}
 
-	return {
-		getCurrentPrice() {
+function executeRequest(method, uri, body) {
+	const serializedBody = body ? JSON.stringify(body) : undefined;
+	const epoch = ~~(new Date() / 1000);
 
-		},
-	};
+	return createSignedHeadersAsync(epoch, method, uri, serializedBody)
+		.then(headers => {
+			return axios({
+				method,
+				url: `${config.host}${uri}`,
+				data: serializedBody,
+				headers,
+			});
+		}).then(resp => resp.data);
+}
+
+module.exports = {
+	getOrders() {
+		return executeRequest('GET', '/orders?status=all');
+	},
 };
+
+module.exports.getOrders()
+	.tap(console.log);
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
+const log = require('./lib/logger');
+const fs = require('fs');
 const exchanges = require('./exchanges');
-const ui = require('./ui');
+//const ui = require('./ui');
+const duration = require('./lib/duration');
+
+//exchanges.getExchange(config.exchange.name, config.exchange.config);
 
 
-const config = require('./rules.example.js');
-exchanges.getExchange(config.exchange.name, config.exchange.config);
+let rules = JSON.parse(fs.readFileSync('rules.json', {encoding: 'utf8'}));
+
+function poll() {
+	log.info('Polling...');
+}
+
+function main() {
+	const period = duration.parse(rules.period);
+	log.info(`Polling every ${period.asMinutes()} minutes...`);
+	poll();
+	setInterval(poll, period.asMilliseconds());
+}
+main();
