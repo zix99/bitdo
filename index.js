906a17c7acae5895bbd896a1ea0f5142e3459641
Christopher LaPointe <cmlapointe11@gmail.com>
2017-09-11 23:57:39 -0400

    Start adding currency conversions

--- index.js
+++ index.js
@@ -59,20 +59,49 @@ function buildExchangeRateTable() {
 		});
 }
 
-function updateHoldings() {
+function getHoldings() {
 	return Promise.map(exchanges, exchange => {
-		return exchange.getHoldings()
-			.then(holdings => {
-				_.each(holdings, holding => plugins.event('onHoldingUpdate', holding));
-				return holdings;
-			});
+		return exchange.getHoldings();
+	}).then(holdings => _.flatten(holdings));
+}
+
+function getRateBetweenCurrencies(rateTable, from, to) {
+	if (to === from)
+		return 1.0;
+
+	const direct = _.get(rateTable, `${from}-${to}`);
+	if (direct !== null)
+		return direct;
+
+	// Maybe we need to go indirectly... try via BTC
+	const toBtc = _.get(rateTable, `${from}-BTC`);
+	const toTarget = _.get(rateTable, `BTC-${to}`);
+	if (toBtc !== null && toTarget !== null)
+		return toBtc * toTarget;
+
+	return 0.0;
+}
+
+function updateHoldings() {
+	return Promise.all([
+		getHoldings(),
+		buildExchangeRateTable(),
+	]).spread((holdings, rates) => {
+		_.each(holdings, holding => {
+			const toBtc = getRateBetweenCurrencies(rates, holding.currency, 'BTC'); //TODO: Configure primary currencies
+			const toUsd = getRateBetweenCurrencies(rates, holding.currency, 'USD');
+
+			holding.balance_btc = holding.balance * toBtc;
+			holding.balance_usd = holding.balance * toUsd;
+
+			console.dir(holding);
+		});
 	});
 }
 
 function poll() {
 	log.info('Polling...');
 	Promise.all([
-		buildExchangeRateTable().tap(console.dir),
 		updateHoldings(),
 	]).then(() => {
 		//ui.update();
